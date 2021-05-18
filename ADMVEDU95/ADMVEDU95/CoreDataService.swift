1e2f87a347a303f13b1d70b9eb3fb0c827ba09b2
osatishur <o.satishur@itransition.com>
2021-05-18 10:49:59 +0300

    filemanager service added

--- ADMVEDU95/ADMVEDU95/CoreDataService.swift
+++ ADMVEDU95/ADMVEDU95/CoreDataService.swift
@@ -14,6 +14,8 @@ class CoreDataService {
         static let entityName = "ResultCoreDataModel"
     }
     
+    private var fileManagerService = FileManagerService()
+    
     lazy var context: NSManagedObjectContext = {
         persistentContainer.viewContext
     }()
@@ -50,7 +52,7 @@ class CoreDataService {
         model.artistName = apiResult.artistName
         model.trackName = apiResult.trackName
         model.kind = apiResult.kind
-        saveFile(url: apiResult.previewUrl ?? "") { fileUrl in
+        fileManagerService.saveFile(url: apiResult.previewUrl ?? "") { fileUrl in
             print("CORE DATA SAVED FILE AT PATH", fileUrl)
             model.previewPath = fileUrl
             self.saveContext()
@@ -58,66 +60,9 @@ class CoreDataService {
         saveContext()
     }
     
-    func saveFile(url: String, completion: @escaping ((_ filePath: String)->())) {
-        guard let url = URL(string: url)  else {
-            return
-        }
-        let fileManager = FileManager.default
-        let documentsDirectoryURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let filePath = documentsDirectoryURL.appendingPathComponent(url.lastPathComponent)
-        do {
-            if try filePath.checkResourceIsReachable() {
-                completion(filePath.absoluteString)
-            } else {
-                downloadFile(withUrl: url, andFilePath: filePath, completion: completion)
-            }
-        } catch {
-            downloadFile(withUrl: url, andFilePath: filePath, completion: completion)
-        }
-    }
-    
-    func downloadFile(withUrl url: URL, andFilePath filePath: URL, completion: @escaping ((_ filePath: String)->())){
-        DispatchQueue.global(qos: .background).async {
-            do {
-                let data = try Data(contentsOf: url)
-                try data.write(to: filePath, options: .atomic)
-                print("saved at \(filePath.absoluteString)")
-                DispatchQueue.main.async {
-                    completion(filePath.absoluteString)
-                }
-            } catch {
-                print("an error happened while downloading or saving the file")
-            }
-        }
-    }
-    
     func deleteAllResults(){
-        deleteDownloadedFiles()
+        fileManagerService.deleteDownloadedFiles()
         deleteEntityObjects()
-    }
-    
-    private func deleteDownloadedFiles() {
-        let fileManager = FileManager.default
-        let documentsDirectoryURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
-        let documentsPath = documentsDirectoryURL?.path
-        
-        do {
-            guard let documentPath = documentsPath else {
-                return
-            }
-            let fileNames = try fileManager.contentsOfDirectory(atPath: "\(documentPath)")
-            print("all files in folder: \(fileNames)")
-            for fileName in fileNames {
-                let filePathName = "\(documentPath)/\(fileName)"
-                try fileManager.removeItem(atPath: filePathName)
-            }
-            
-            let files = try fileManager.contentsOfDirectory(atPath: "\(documentPath)")
-            print("all files after deleting: \(files)")
-            
-        } catch {
-            print("Could not clear folder: \(error)")
-        }
     }
     
     private func deleteEntityObjects() {
---
+++ ADMVEDU95/ADMVEDU95/FileManagerService.swift
@@ -0,0 +1,69 @@
+//
+//  FileManagerService.swift
+//  ADMVEDU95
+//
+//  Created by Satishur, Oleg on 17.05.2021.
+//
+
+import Foundation
+
+class FileManagerService {
+    
+    func saveFile(url: String, completion: @escaping ((_ filePath: String)->())) {
+        guard let url = URL(string: url)  else {
+            return
+        }
+        let fileManager = FileManager.default
+        let documentsDirectoryURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let filePath = documentsDirectoryURL.appendingPathComponent(url.lastPathComponent)
+        do {
+            if try filePath.checkResourceIsReachable() {
+                completion(filePath.absoluteString)
+            } else {
+                downloadFile(withUrl: url, andFilePath: filePath, completion: completion)
+            }
+        } catch {
+            downloadFile(withUrl: url, andFilePath: filePath, completion: completion)
+        }
+    }
+    
+    func downloadFile(withUrl url: URL, andFilePath filePath: URL, completion: @escaping ((_ filePath: String)->())){
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try Data(contentsOf: url)
+                try data.write(to: filePath, options: .atomic)
+                print("saved at \(filePath.absoluteString)")
+                DispatchQueue.main.async {
+                    completion(filePath.absoluteString)
+                }
+            } catch {
+                print("an error happened while downloading or saving the file")
+            }
+        }
+    }
+    
+    func deleteDownloadedFiles() {
+        let fileManager = FileManager.default
+        let documentsDirectoryURL =  fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        let documentsPath = documentsDirectoryURL?.path
+        
+        do {
+            guard let documentPath = documentsPath else {
+                return
+            }
+            let fileNames = try fileManager.contentsOfDirectory(atPath: "\(documentPath)")
+            print("all files in folder: \(fileNames)")
+            for fileName in fileNames {
+                let filePathName = "\(documentPath)/\(fileName)"
+                try fileManager.removeItem(atPath: filePathName)
+            }
+            
+            let files = try fileManager.contentsOfDirectory(atPath: "\(documentPath)")
+            print("all files after deleting: \(files)")
+            
+        } catch {
+            print("Could not clear folder: \(error)")
+        }
+    }
+    
+}
